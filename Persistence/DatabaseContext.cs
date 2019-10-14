5431e6dc688acfaa5c55650ef732e6fb7c6a626a
Daniel Agg <daniel.agg@outlook.com>
2019-10-14 19:51:56 +0200

    Data seeding

--- Persistence/DatabaseContext.cs
+++ Persistence/DatabaseContext.cs
@@ -22,18 +22,15 @@ namespace Persistence
                 relationship.DeleteBehavior = DeleteBehavior.Restrict;
             }
 
-            modelBuilder.Entity<User>().HasData(new[]
-            {
-               new User
-               {
-                   Id = Guid.NewGuid(),
-                   FirstName = "Daniel",
-                   LastName = "Agg",
-                   CreatedOn = DateTime.Parse("2016-06-13T17:41:32.6436241Z"),
-                   LastAccessOn = DateTime.Parse("2019-10-14T19:31:11.5359553Z")
-               }
-            });
+            PopulateUsers(modelBuilder);
+            PopulateArtists(modelBuilder);
+            PopulateAlbums(modelBuilder);
+            PopulateTracks(modelBuilder);
+            PopulatePlaylists(modelBuilder);
+            PopulateTrackPlaylistAssignments(modelBuilder);
         }
+
+        #region entity definitions
 
         public DbSet<User> Users { get; set; }
         public DbSet<Artist> Artists { get; set; }
@@ -44,7 +41,7 @@ namespace Persistence
 
         public class User
         {
-            public Guid Id { get; set; }
+            public int Id { get; set; }
             public string FirstName { get; set; }
             public string LastName { get; set; }
             public DateTime LastAccessOn { get; set; }
@@ -53,15 +50,15 @@ namespace Persistence
 
         public class Artist
         {
-            public Guid Id { get; set; }
+            public int Id { get; set; }
             public string Name { get; set; }
             public DateTime CreatedOn { get; set; }
         }
 
         public class Album
         {
-            public Guid Id { get; set; }
-            public Guid ArtistId { get; set; }
+            public int Id { get; set; }
+            public int ArtistId { get; set; }
             public virtual Artist Artist { get; set; }
             public string Name { get; set; }
             public DateTime CreatedOn { get; set; }
@@ -69,8 +66,8 @@ namespace Persistence
 
         public class Track
         {
-            public Guid Id { get; set; }
-            public Guid AlbumId { get; set; }
+            public int Id { get; set; }
+            public int AlbumId { get; set; }
             public virtual Album Album { get; set; }
             public string Name { get; set; }
             public bool IsExplicit { get; set; }
@@ -79,21 +76,163 @@ namespace Persistence
 
         public class Playlist
         {
-            public Guid Id { get; set; }
+            public int Id { get; set; }
             public string Name { get; set; }
-            public Guid UserId { get; set; }
+            public int UserId { get; set; }
             public virtual User User { get; set; }
             public DateTime CreatedOn { get; set; }
         }
 
         public class TrackPlaylistAssignment
         {
-            public Guid Id { get; set; }
-            public Guid PlaylistId { get; set; }
+            public int Id { get; set; }
+            public int PlaylistId { get; set; }
             public virtual Playlist Playlist { get; set; }
-            public Guid TrackId { get; set; }
+            public int TrackId { get; set; }
             public virtual Track Track { get; set; }
             public DateTime AddedTrackToPlaylistOn { get; set; }
         }
+
+        #endregion
+
+        #region data seeding
+
+        private void PopulateUsers(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(new[]
+            {
+               new User
+               {
+                   Id = 1,
+                   FirstName = "Daniel",
+                   LastName = "Agg",
+                   CreatedOn = DateTime.Parse("2016-06-13T17:41:32.6436241Z"),
+                   LastAccessOn = DateTime.Parse("2019-10-14T19:31:11.5359553Z")
+               }
+            });
+        }
+
+        private void PopulateArtists(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Artist>().HasData(new[]
+{
+               new Artist
+               {
+                   Id = 1,
+                   Name = "Queens of the Stone Age",
+                   CreatedOn = DateTime.Parse("1990-01-01T12:00:00.0000000Z"),
+               },
+               new Artist
+               {
+                   Id = 2,
+                   Name = "Led Zeppelin",
+                   CreatedOn = DateTime.Parse("1990-01-01T12:00:00.0000000Z"),
+               },
+               new Artist
+               {
+                   Id = 3,
+                   Name = "Black Sabbath",
+                   CreatedOn = DateTime.Parse("1990-01-01T12:00:00.0000000Z"),
+               },
+               new Artist
+               {
+                   Id = 4,
+                   Name = "Gary Clark Jr.",
+                   CreatedOn = DateTime.Parse("1990-01-01T12:00:00.0000000Z"),
+               },
+               new Artist
+               {
+                   Id = 5,
+                   Name = "Them Crooked Vultures",
+                   CreatedOn = DateTime.Parse("1990-01-01T12:00:00.0000000Z"),
+               },
+               new Artist
+               {
+                   Id = 6,
+                   Name = "Black Keys",
+                   CreatedOn = DateTime.Parse("1990-01-01T12:00:00.0000000Z"),
+               },
+               new Artist
+               {
+                   Id = 7,
+                   Name = "Killing Joke",
+                   CreatedOn = DateTime.Parse("1990-01-01T12:00:00.0000000Z"),
+               },
+               new Artist
+               {
+                   Id = 8,
+                   Name = "Black Rebel Motorcycle Club",
+                   CreatedOn = DateTime.Parse("1990-01-01T12:00:00.0000000Z"),
+               }
+            });
+        }
+
+        private void PopulateAlbums(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Album>().HasData(new[]
+            {
+               new Album
+               {
+                   Id = 1,
+                   ArtistId = 1,
+                   Name = "Villains",
+                   CreatedOn = DateTime.Parse("1990-01-01T12:00:00.0000000Z")
+               },
+               new Album
+               {
+                   Id = 2,
+                   ArtistId = 1,
+                   Name = "...Like Clockwork",
+                   CreatedOn = DateTime.Parse("1990-01-01T12:00:00.0000000Z")
+               }
+            });
+        }
+
+        private void PopulateTracks(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        private void PopulatePlaylists(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Playlist>().HasData(new[]
+            {
+               new Playlist
+               {
+                   Id = 1,
+                   Name = "Walk Like a Badass",
+                   UserId = 1,
+                   CreatedOn = DateTime.Parse("2019-10-13T18:12:32.0000000Z")
+               },
+               new Playlist
+               {
+                   Id = 2,
+                   Name = "Mellow Rock",
+                   UserId = 1,
+                   CreatedOn = DateTime.Parse("2019-08-22T10:52:18.0000000Z")
+               },
+               new Playlist
+               {
+                   Id = 3,
+                   Name = "Music to Focus",
+                   UserId = 1,
+                   CreatedOn = DateTime.Parse("2018-12-30T14:02:25.0000000Z")
+               },
+               new Playlist
+               {
+                   Id = 4,
+                   Name = "Oldschool Goodness",
+                   UserId = 1,
+                   CreatedOn = DateTime.Parse("2018-10-05T08:36:10.0000000Z")
+               }
+            });
+        }
+
+        private void PopulateTrackPlaylistAssignments(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        #endregion
     }
 }
