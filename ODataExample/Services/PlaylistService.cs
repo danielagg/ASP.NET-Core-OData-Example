19c8bd03d32b4ffcac062807cb21db3c39daa991
Daniel Agg <daniel.agg@outlook.com>
2019-10-16 21:08:48 +0200

    Implement GetPlaylistsOfUser

--- ODataExample/Services/PlaylistService.cs
+++ ODataExample/Services/PlaylistService.cs
@@ -19,15 +19,45 @@ namespace WebAPI.Services
 
         public async Task<UserPlaylist[]> GetPlaylistsOfUser(int userId)
         {
-            var result = await dbContext.Playlists
-                .Where(p => p.UserId == userId)
-                .Select(p => new UserPlaylist
+            var result = await dbContext.TrackPlaylistAssignments
+                .Where(tpa => tpa.Playlist.UserId == userId)
+                .Select(tpa => new
                 {
-                    Id = p.Id,
-                    Name = p.Name,
-                    CreatedOn = p.CreatedOn,
-                    ModifiedOn = p.ModifiedOn,
-                    OwnerUserName = $"{p.User.FirstName} {p.User.LastName}"
+                    tpa.Playlist.Id,
+                    tpa.Playlist.Name,
+                    tpa.Playlist.CreatedOn,
+                    tpa.Playlist.ModifiedOn,
+                    OwnerUserName = $"{tpa.Playlist.User.FirstName} {tpa.Playlist.User.LastName}",
+                    TrackInfo = new
+                    {
+                        tpa.Track.Id,
+                        Title = tpa.Track.Name,
+                        WrittenAt = tpa.Track.CreatedOn,
+                        Artist = tpa.Track.Album.Artist.Name
+                    }
+                })
+                .GroupBy(
+                    k => new { k.Id, k.Name, k.CreatedOn, k.ModifiedOn, k.OwnerUserName },
+                    v => v.TrackInfo,
+                    (k, v) => new
+                    {
+                        PlaylistInfo = k,
+                        Tracks = v
+                    })
+                .Select(r => new UserPlaylist
+                {
+                    Id = r.PlaylistInfo.Id,
+                    Name = r.PlaylistInfo.Name,
+                    CreatedOn = r.PlaylistInfo.CreatedOn,
+                    ModifiedOn = r.PlaylistInfo.ModifiedOn,
+                    OwnerUserName = r.PlaylistInfo.OwnerUserName,
+                    Tracks = r.Tracks.Select(t => new Track
+                    {
+                        Id = t.Id,
+                        SongName = t.Title,
+                        ArtistName = t.Artist,
+                        WrittenAt = t.WrittenAt
+                    }).ToArray()
                 })
                 .ToArrayAsync();
 
